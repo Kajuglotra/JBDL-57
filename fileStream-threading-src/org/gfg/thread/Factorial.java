083e47d75980f6cde881a941d18101f1803f9430
kajal <51613822+Kajuglotra@users.noreply.github.com>
2023-08-20 17:31:54 +0530

    Thread and file streaming

---
+++ fileStream-threading-src/org/gfg/thread/Factorial.java
@@ -0,0 +1,24 @@
+package org.gfg.thread;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[]{1,2,3,4,5,6,7,8,9,10};
+        Integer[] arr2 = new Integer[]{10000,20000,30000,40000,50000,60000,70000,80000,90000,100000};
+        long startTime = System.currentTimeMillis();
+
+        Arrays.stream(arr2).parallel().map(i-> getFactorial(i)).forEach(i-> System.out.println(i));
+        long endTime = System.currentTimeMillis();
+        System.out.println("time taken : " +(endTime-startTime));
+
+    }
+    public static BigInteger getFactorial(int num){
+        BigInteger result = BigInteger.ONE;
+        for(int i = 2; i<= num ; i++){
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+}
