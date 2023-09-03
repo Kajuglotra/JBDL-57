c6db3dd262be8f8f5d6ccb27da629ca6bf1908dc
kajal <51613822+Kajuglotra@users.noreply.github.com>
2023-09-03 09:10:54 +0530

    Delete fileStream-threading-src directory

--- fileStream-threading-src/org/gfg/thread/flightSearch/AirlineFlightSearchInterface.java
+++
@@ -1,8 +0,0 @@
-package org.gfg.thread.flightSearch;
-
-import java.util.List;
-
-public interface AirlineFlightSearchInterface {
-
-    public List<FlightData> getAllFlightFromSrcToDest(String src, String dest);
-}
